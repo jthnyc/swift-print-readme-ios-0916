747f27fdcf76248ed15975774fd90a4fd2cd6c07
Joanna Tzu-Hsuan Huang <joannathhuang@Joannas-MacBook-Pro.local>
2016-08-29 17:48:15 -0400

    done

---
+++ Print Lab.playground/Contents.swift
@@ -0,0 +1,16 @@
+var favoriteWord = "bazinga"    // Use your own favorite word!
+var favoriteColor = "blue"        // Use your own favorite color!
+var favoriteMusician = "Moderat"  // Use your own favorite musician!
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+print("I'm printing a string")
+print("printing long sentences is no problem. green eggs and ham.")
+
+let color = "blue"
+print("The sky is \(color).")
+print("Hey! My favorite word is \(favoriteWord), my faovrite color is \(favoriteColor), my favorite musician is \(favoriteMusician).")
+
+var skySentence = "The sky is \(color)."
+print(skySentence)
